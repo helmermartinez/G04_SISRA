884a93464f064b268ef4b44958ae471a8972b0da
michelle <viviansolis@upeu.edu.pe>
2020-12-07 21:05:50 +0100

    seguridad

--- src/test/java/pe/edu/upeu/SISRA/SisraApplicationTests.java
+++ src/test/java/pe/edu/upeu/SISRA/SisraApplicationTests.java
@@ -4,35 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import com.google.gson.Gson;
 
-import pe.edu.upeu.SISRA.dao.AsociacionDao;
-import pe.edu.upeu.SISRA.dao.DocumentoDao;
 import pe.edu.upeu.SISRA.dao.RolDao;
 import pe.edu.upeu.SISRA.dao.UsuarioDao;
+import pe.edu.upeu.SISRA.entity.Rol;
 
 @SpringBootTest
 class SisraApplicationTests {
-	@Autowired
-	AsociacionDao asc;
-	@Autowired
-	DocumentoDao d;
-	@Autowired
-	PasswordEncoder p;
-	@Autowired
-	RolDao r;
-	@Autowired
-	UsuarioDao u;
 	
+	@Autowired
+	PasswordEncoder passwordEncoder;
 	@Test
 	void contextLoads() {
-		
-		/*System.out.println(asc.readAll());*/
-		
-		/*System.out.println(d.read(1));*/
-		
-		/*System.out.println(p.encode("1234"));*/
-		
-		System.out.println(r.buscarRolUser(6));
+		System.out.println(passwordEncoder.encode("123"));
 	}
-
 }
